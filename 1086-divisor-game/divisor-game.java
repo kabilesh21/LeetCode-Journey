a3bd0594e47ef0c907d88072f93aeed8c6fb8148
KABILESH M <kabileshclg0678@gmail.com>
2026-09-18 22:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (32.69%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        boolean k=false;
+        
+        if(n%2==0)
+        {
+            k=true;
+        }
+        return k;
+    }
+}
